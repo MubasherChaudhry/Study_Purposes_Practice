6a85dafbc4c3c46b9fc978716b2bf31ef0c6afa0
Mubasher <mobi_20_11@yahoo.com>
2023-03-11 00:01:41 +0100

    Adding Basic data

---
+++ basic-study/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <nav>
+    <li>Home</li>
+    <li> About </li>
+    <li>Contact</li>
+    </nav>
+    <img src="" alt="" />
+    <div className="container">
+    <h1>Hello Mobi</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quidem quasi veritatis, recusandae neque voluptatem at corporis temporibus inventore, doloribus cumque molestias quaerat optio ad vitae consequatur commodi dignissimos. Sequi.
+    Voluptas iure, molestiae sequi, aliquid iste temporibus ipsa atque magnam rem, obcaecati officiis asperiores illum impedit ipsum ab nesciunt! Soluta molestias quae ipsa molestiae. Perspiciatis repudiandae quia sunt ad fugit?</p>
+
+    </div>
+  
+  </>
+
+);
+}
+
+export default App;
