729418b87d8819ae4ceadc19e09a5372adcc3d94
Mubasher <mobi_20_11@yahoo.com>
2023-03-11 00:23:30 +0100

    Boot strap added

--- basic-study/src/App.js
+++ basic-study/src/App.js
@@ -4,19 +4,42 @@ import './App.css';
 function App() {
   return (
   <>
-  <nav>
-    <li>Home</li>
-    <li> About </li>
-    <li>Contact</li>
-    </nav>
-    <img src="" alt="" />
-    <div className="container">
-    <h1>Hello Mobi</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quidem quasi veritatis, recusandae neque voluptatem at corporis temporibus inventore, doloribus cumque molestias quaerat optio ad vitae consequatur commodi dignissimos. Sequi.
-    Voluptas iure, molestiae sequi, aliquid iste temporibus ipsa atque magnam rem, obcaecati officiis asperiores illum impedit ipsum ab nesciunt! Soluta molestias quae ipsa molestiae. Perspiciatis repudiandae quia sunt ad fugit?</p>
-
+  <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
-  
+  </div>
+</nav>
   </>
 
 );
