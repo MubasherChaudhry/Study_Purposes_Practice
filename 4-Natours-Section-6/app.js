de1d7f1a97aec7512935cac292141bb5394309a8
Mubasher <mobi_20_11@yahoo.com>
2023-05-21 01:03:50 +0200

    updated with env

--- 4-Natours-Section-6/app.js
+++ 4-Natours-Section-6/app.js
@@ -5,41 +5,17 @@ const userRouter=require('./routes/userRoutes')
 
 const app = express();
 
-//MIDDLEWARES
+//Middleware
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log('Hello its your own middleWare');
   next();
 });
 
-/* app.use((req, res, next) => {
-  req.requesTime = new Date().toISOString();
-  next();
-}); */
-
-/* app.get('/',(req,res)=>{
-  res
-  .status(200)
-  .json({message:'hello from ther serverside', newStep:'into Natours'})
-})
-app.post('/',(req,res)=>{
-  res
-  .status(200)
-  .json({message:'you can reach to this post request endpoint',newRequest:'this is First post request endpoint'})
-}) */
-
-//2:- Rourte HANDLERS
-// app.get('/api/v1/tours',getAlltours );
-// app.post('/api/v1/tours',createTour);
-// app.get('/api/v1/tours/:id',getTour );
-// app.patch('/api/v1/tours/:id',updateTour)
-// app.delete('/api/v1/tours/:id',deleteTour)
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
-
-//4:- START SERVER
 
 module.exports= app
--- 4-Natours-Section-6/server.js
+++ 4-Natours-Section-6/server.js
@@ -1,6 +1,13 @@
+const dotenv=require('dotenv')
 const app =require('./app')
+
+dotenv.config({path:'./config.env'})
+
+console.log(process.env);
+// console.log(app.get('env')); use this to define development environment 
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port:${PORT}`);
 });
+
