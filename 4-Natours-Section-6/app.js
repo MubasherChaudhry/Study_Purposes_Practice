74532a3bf9580104494958a1b6e749851fcd916c
Mubasher <mobi_20_11@yahoo.com>
2023-05-21 00:15:00 +0200

    middleware chaininig

--- 4-Natours-Section-6/app.js
+++ 4-Natours-Section-6/app.js
@@ -1,24 +1,24 @@
-const fs = require('fs');
 const express = require('express');
-const morgan =require('morgan')
+const morgan = require('morgan');
+const tourRouter=require('./routes/tourRoutes')
+const userRouter=require('./routes/userRoutes')
 
 const app = express();
-const PORT = 3000;
 
 //MIDDLEWARES
-app.use(morgan('dev'))
-
+app.use(morgan('dev'));
 app.use(express.json());
-app.use((req,res,next)=>{
-console.log('Hello its your own middleWare')
-next()
-})
 
-app.use((req,res,next)=>{
-  req.requesTime=new Date().toISOString()
-  next()
-  })
 
+app.use((req, res, next) => {
+  console.log('Hello its your own middleWare');
+  next();
+});
+
+/* app.use((req, res, next) => {
+  req.requesTime = new Date().toISOString();
+  next();
+}); */
 
 /* app.get('/',(req,res)=>{
   res
@@ -31,110 +31,15 @@ app.post('/',(req,res)=>{
   .json({message:'you can reach to this post request endpoint',newRequest:'this is First post request endpoint'})
 }) */
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
 //2:- Rourte HANDLERS
-const getAlltours=(req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-}
-const getTour=(req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (id > tours.length) {
-    // if (!tour)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-
-    data: {
-      tour,
-    },
-  });
-}
-const createTour= (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id * 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-}
-const updateTour= (req,res)=>{
-  if (req.params.id *1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-res
-.status(200)
-.json({
-  status:'success',
-  data:{
-    tours:'<Updating tour here....>'
-  }
-})
-}
-const deleteTour=(req,res)=>{
-  if (req.params.id *1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-res
-.status(204)
-.json({
-  status:'success',
-  data:null
-})
-}
-
 // app.get('/api/v1/tours',getAlltours );
 // app.post('/api/v1/tours',createTour);
 // app.get('/api/v1/tours/:id',getTour );
 // app.patch('/api/v1/tours/:id',updateTour)
 // app.delete('/api/v1/tours/:id',deleteTour)
-
-//3:-Routes
-
-app.route('/api/v1/tours')
-.get(getAlltours)
-.post(createTour)
-
-app.route('/api/v1/tours/:id')
-.get(getTour)
-.patch(updateTour)
-.delete(deleteTour)
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
 
 //4:- START SERVER
 
-app.listen(PORT, () => {
-  console.log(`App is running on port:${PORT}`);
-});
+module.exports= app
---
+++ 4-Natours-Section-6/controller/userController.js
@@ -0,0 +1,33 @@
+const express=require('express')
+
+
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'this route is not yet define',
+  });
+};
+exports.createUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'this route is not yet define',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'this route is not yet define',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'this route is not yet define',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'this route is not yet define',
+  });
+};
