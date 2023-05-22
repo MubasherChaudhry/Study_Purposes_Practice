7cd34fa59ae56c6ab844726325684321f14170cd
Mubasher <mobi_20_11@yahoo.com>
2023-05-22 14:26:08 +0200

    structuring better

--- 4-Natours-Section-6/server.js
+++ 4-Natours-Section-6/server.js
@@ -17,39 +17,6 @@ mongoose.connect(DB,{
 
 })
 .then(()=>console.log('DB connection Successful'))
-
-const tourSchema= new mongoose.Schema({
-  name:{
-    type:String,
-    required:[true,'Atour Must Have a name'],
-    unique:true},
-  
-    rating:{
-      type:Number,
-      default:4.5
-    },
-
-  price:{
-    type:Number,
-  required:[true, 'A tour must have a price']} 
-
-})
-const Tour=mongoose.model('Tour',tourSchema)
-
-const testTour = new Tour({
-name:'the forest Biker',
-rating:4.7,
-price:497
-})
-
-testTour
-.save()
-.then(doc=>{
-console.log(doc);
-})
-.catch(err=>{
-console.log('ERROR',err);
-})
 // console.log(app.get('env')); use this to define development environment
 
 const port = process.env.PORT || 5050;
