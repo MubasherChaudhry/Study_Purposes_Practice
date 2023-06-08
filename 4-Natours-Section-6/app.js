37a8e4722284203c444d5ca61cfc2cfab88f8ba8
Mubasher <mobi_20_11@yahoo.com>
2023-06-08 22:55:27 +0200

    before implimenting global erro handler

--- 4-Natours-Section-6/app.js
+++ 4-Natours-Section-6/app.js
@@ -9,16 +9,23 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello its your own middleWare');
+  req.reqTime = new Date().toISOString();
   next();
 });
 
-app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this Server`,
+  });
+});
 
 module.exports = app;
--- 4-Natours-Section-6/models/tourModel.js
+++ 4-Natours-Section-6/models/tourModel.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: [true, 'A tour Must Have a name'],
       unique: true,
+      trim: true,
+      maxlength: [40, 'a tour must have less or equal to 40 characters'],
+      minlength: [10, 'a tour must have more or equal to 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
@@ -19,10 +25,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour Must Have a Difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty iseither:easy medium or difficult ',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'rating must be above 1.0'],
+      max: [1, 'rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -32,8 +44,13 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
-
+    priceDiscount: {
+      type: Number,
+      validator: function (val) {
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below the regular price',
+    },
     summary: {
       type: String,
       trim: true,
@@ -85,9 +102,26 @@ tourSchema.pre('save', function (next) {
 
 //QUERY MIDDLEWARE
 
-tourSchema.pre('find', function (next) {
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
   next();
 });
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
