0ee26570b6e8f91e3ef06eb6e039dc04687f87a9
Mubasher <mobi_20_11@yahoo.com>
2023-06-12 00:45:02 +0200

    global Error Handler Ćreated

--- 4-Natours-Section-6/app.js
+++ 4-Natours-Section-6/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
 
@@ -22,24 +24,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this Server`,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this Server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next()
+  next(new AppError(`Can't find ${req.originalUrl} on this Server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
