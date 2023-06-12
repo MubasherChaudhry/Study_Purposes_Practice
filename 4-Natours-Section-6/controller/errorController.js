370642ad37493eba758ae94f5ed482287b4335ba
Mubasher <mobi_20_11@yahoo.com>
2023-06-12 15:23:10 +0200

    errors differenciated

--- 4-Natours-Section-6/controller/errorController.js
+++ 4-Natours-Section-6/controller/errorController.js
@@ -1,9 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational Error , trusted error:send message to clint
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other unknown error:don't leak error details
+  } else {
+    //1) log error
+    console.log('error', err);
+
+    //2) long error
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very Wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
