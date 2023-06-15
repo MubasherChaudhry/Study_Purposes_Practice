c84a81b28eec23c6341effb0eb68bbf26ce307ce
Mubasher <mobi_20_11@yahoo.com>
2023-06-15 14:35:00 +0200

    token expire or invalid  error implimented

--- 4-Natours-Section-6/controllers/authController.js
+++ 4-Natours-Section-6/controllers/authController.js
@@ -3,54 +3,49 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-
+//create User
 const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+  return token;
 };
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
+    passwordConfirm: req.body.passwordConfirm,
   });
 
   const token = signToken(newUser._id);
 
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  res.status(201).json({ status: 'Molodets', token, data: { usr: newUser } });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
-
-  //1)check if email and password exist
+  //1) Check if email and password exist
   if (!email || !password) {
-    return next(new AppError('please provide email and password', 400));
+    return next(new AppError('Please provide email and password', 400));
   }
-  //2)check if user exist and password is correct
-  const user = await User.findOne({ email }).select('+password');
+  //2) check if user exist & password is correct
+  const user = await User.findOne({ email }).select('+password'); // + to show the property that you need
+  //const correct = await user.correctPassword(password, user.password);
+
+  // If the first operand evaluates to true, the || operator immediately returns the value of the first operand without evaluating the second operand. This is known as short-circuiting.
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401));
   }
 
-  //3)if everything is of then send token to clint
+  //3) If everything is OK send Token to client
   const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  res.status(200).json({ status: 'Success', token });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
-  //1) getting the token  and checking if its exist
+  //1) Getting Token and check if it exist
   let token;
   if (
     req.headers.authorization &&
@@ -58,19 +53,17 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  // console.log(token);
-
+  console.log(token);
   if (!token) {
-    return next(new AppError('You are not logged in', 401));
+    return next(
+      new AppError('You are not log In! Please log in to get access')
+    );
   }
-  //2)Verification token
+  //2) Verification Token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  next();
   console.log(decoded);
+  //3) Check if User still exists
 
-  //3)check if the user still exists
-
-  //4)check if user changed password after the token was issued
-
+  //4)check if user change password after JWT/Token was issued
   next();
 });
--- 4-Natours-Section-6/controllers/tourController.js
+++ 4-Natours-Section-6/controllers/tourController.js
@@ -1,7 +1,8 @@
+//const { query } = require('express');
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
-const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -9,135 +10,96 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
-/* exports.checkID=(req,res,next,val)=>{
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-  next()
-} */
-
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or Price ',
-//     });
-//   }
-//   next();
-// };
 
 exports.getAllTours = catchAsync(async (req, res, next) => {
-  // EXECUTE QUERY
+  //Build the Query
+  console.log(req.query);
+
+  //Execute the Query
   const features = new APIFeatures(Tour.find(), req.query)
     .filter()
     .sort()
     .limitFields()
     .paginate();
   const tours = await features.query;
+  //query.sort().select().skip().limit()
 
-  // SEND RESPONSE
+  ///Send Response
   res.status(200).json({
     status: 'success',
     results: tours.length,
-    data: {
-      tours,
-    },
+    data: { tours },
   });
 });
+/// Reading
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
-
+  //Tour.findONe({_id: req.params.id}) -> same way from above, we can query for that <field></field>
   if (!tour) {
-    return next(new AppError(`No Tour found with that ID, 404`));
+    return next(new AppError('No tours found with that ID', 404));
   }
-
   res.status(200).json({
     status: 'success',
     data: { tour },
   });
 });
 
+////Creating
 exports.createTour = catchAsync(async (req, res, next) => {
   const newTour = await Tour.create(req.body);
   res.status(201).json({
     status: 'success',
-    data: {
-      tour: newTour,
-    },
+    data: { tour: newTour },
   });
 });
+
+///////Update
 exports.updateTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
-
   if (!tour) {
-    return next(new AppError(`No Tour found with that ID, 404`));
+    return next(new AppError('No tours found with that ID', 404));
   }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  res.status(200).json({ status: 'success', data: { tour } });
 });
-
+///////Delete
 exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
-
   if (!tour) {
-    return next(new AppError(`No Tour found with that ID, 404`));
+    return next(new AppError('No tours found with that ID', 404));
   }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+  //204- No Content
+  res.status(204).json({ status: 'success', data: null });
 });
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
-    {
-      $match: { ratingsAverage: { $gte: 4.5 } },
-    },
+    { $match: { ratingAverage: { $gte: 4.5 } } },
     {
       $group: {
-        _id: { $toUpper: '$difficulty' },
+        // _id: '$ratingAverage',
+        _id: { $toUpper: '$difficulty' }, // sort them by this property
         numTours: { $sum: 1 },
-        numRatings: { $sum: '$ratingsQuantity' },
-
-        avgRatting: { $avg: '$ratingsAverage' },
+        numRatings: { $sum: '$ratingQuantity' },
+        avgRating: { $avg: '$ratingAverage' },
         avgPrice: { $avg: '$price' },
         minPrice: { $min: '$price' },
         maxPrice: { $max: '$price' },
       },
     },
-    {
-      $sort: { avgPrice: 1 },
-    },
-    // {
-    //   $match: { _id: { $ne: 'EASY' } },
-    // },
+    { $sort: { avgPrice: 1 } },
+    // { $match: { _id: { $ne: 'EASY' } } },// exclude tours that are easy
   ]);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      stats,
-    },
-  });
+  res.status(200).json({ status: 'success', data: { stats } });
 });
 
 exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
-  const year = req.params.year * 1; //2021
+  const year = req.params.year * 1;
   const plan = await Tour.aggregate([
-    {
-      $unwind: '$startDates',
-    },
+    { $unwind: '$startDates' }, // one document for each of the dates
     {
       $match: {
         startDates: {
@@ -154,7 +116,9 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
       },
     },
     {
-      $addFields: { month: '$_id' },
+      $addFields: {
+        month: '$_id',
+      },
     },
     {
       $project: {
@@ -162,17 +126,9 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
       },
     },
     {
-      $sort: { numTourStarts: -1 },
+      $sort: { numTourStarts: -1, month: 1 }, // month was added from me not from John
     },
-    {
-      $limit: 6,
-    },
+    { $limit: 12 },
   ]);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      plan,
-    },
-  });
+  res.status(200).json({ status: 'success', data: { plan } });
 });
