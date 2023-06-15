bb3e4adea977f351460d33dc30165abd46457ef1
Mubasher <mobi_20_11@yahoo.com>
2023-06-15 11:48:56 +0200

    implemented access to specific route with token1

--- 4-Natours-Section-6/controllers/authController.js
+++ 4-Natours-Section-6/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -57,12 +58,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log(token);
+  // console.log(token);
 
   if (!token) {
     return next(new AppError('You are not logged in', 401));
   }
   //2)Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  next();
+  console.log(decoded);
 
   //3)check if the user still exists
 
