8a5897078cde8f1eda08e0935cc02e420077b786
Mubasher <mobi_20_11@yahoo.com>
2023-06-20 01:04:06 +0200

    updatecurrent useragain

--- 4-Natours-Section-6/controllers/userController.js
+++ 4-Natours-Section-6/controllers/userController.js
@@ -3,6 +3,14 @@ const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await features.query;
 
@@ -16,7 +24,7 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   //1) Create error if user post password data
   if (req.body.password || req.body.confirmPassword) {
     return next(
@@ -27,12 +35,21 @@ exports.updateMe = (req, res, next) => {
     );
   }
 
-  //2) update user document
+  //2)filtered out unwanted fields name that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  //3) update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     status: 'success',
+    data: {
+      user: updatedUser,
+    },
   });
-};
+});
 
 exports.createUsers = (req, res) => {
   res.status(201).json({
