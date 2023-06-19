a801828ae5f7d3b623368a22b722fe3dd0b01d38
Mubasher <mobi_20_11@yahoo.com>
2023-06-19 22:34:24 +0200

    resset password is done

--- 4-Natours-Section-6/controllers/authController.js
+++ 4-Natours-Section-6/controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -146,9 +147,30 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = catchAsync(async (req, res, next) => {
   //1) get user based on token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
   //2) if token is not expire, and there is a user , set a new password
+  if (!user) {
+    return next(new AppError('Token is not Valid or expired', 400));
+  }
+  user.password = req.body.password;
+  user.confirmPassword = req.body.confirmPassword;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
   //3) update change Password at property of user
+  
+  
   //4) Log the user in , send JWT
-};
+  const token = signToken(user._id);
+  res.status(200).json({ status: 'Success', token });
+});
