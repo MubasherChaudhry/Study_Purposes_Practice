e7fe7a30d9a03cba56a353c2e78eb36c2ac91002
Mubasher <mobi_20_11@yahoo.com>
2023-06-24 23:42:29 +0200

    factory function create and update+

--- 4-Natours-Section-6/controllers/userController.js
+++ 4-Natours-Section-6/controllers/userController.js
@@ -73,10 +73,7 @@ exports.getUser = (req, res) => {
     message: 'this route is not yet define',
   });
 };
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'Error',
-    message: 'this route is not yet define',
-  });
-};
+
+//Do Not update password with this
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
