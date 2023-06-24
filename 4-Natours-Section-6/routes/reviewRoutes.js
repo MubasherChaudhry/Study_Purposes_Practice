f5881989d8649907ec6cea1b9e02728a2e7eadfd
Mubasher <mobi_20_11@yahoo.com>
2023-06-24 21:42:15 +0200

    get all revires on tour

--- 4-Natours-Section-6/routes/reviewRoutes.js
+++ 4-Natours-Section-6/routes/reviewRoutes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
+//POST /tour/tourId/reviews
+//GET /tour/tourId/reviews
+// POST/reviews
+
+//above both routes will be end-up in same route which is post route below
 router
   .route('/')
   .get(reviewController.getAllReviews)
